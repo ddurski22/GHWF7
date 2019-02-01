6e84849888d83ee9e974be2a541fed1ed83c5a98
David <stdnd19@gmail.com>
2019-02-01 15:30:56 -0500

    added the ability to drop lowest grades

--- grades.py
+++ grades.py
@@ -1,5 +1,14 @@
 
-def compute_hw_average(grades):
+def compute_hw_average(grades,drop):
     if len(grades) == 0:
         return 0
+    low_grade = 100
+    count = 0
+    while(count<drop):
+        low_grade = 100
+        for grade in grades:
+            if grade <= low_grade:
+                low_grade = grade
+        grades.remove(low_grade)
+        count+=1
     return sum(grades)/len(grades)
--- test_grades.py
+++ test_grades.py
@@ -4,13 +4,18 @@ from grades import compute_hw_average
 
 def test_zero_grades():
     grades = []
-    assert compute_hw_average(grades) == 0
+    assert compute_hw_average(grades,0) == 0
 
 
 def test_single_grade():
     grades = [42]
-    assert compute_hw_average(grades) == 42
+    assert compute_hw_average(grades,0) == 42
 
 def test_two_grade():
         grades = [0,50]
-        assert compute_hw_average(grades) == 25
+        assert compute_hw_average(grades,0) == 25
+
+
+def test_drop_grade():
+    grades = [0, 50, 100]
+    assert compute_hw_average(grades, 2) == 100
