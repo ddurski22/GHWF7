2d599e21cead37f2f4d1e91f48c4a9fba503ab76
David <stdnd19@gmail.com>
2019-02-01 15:34:34 -0500

    created better variable names

--- grades.py
+++ grades.py
@@ -2,13 +2,13 @@
 def compute_hw_average(grades,drop):
     if len(grades) == 0:
         return 0
-    low_grade = 100
+    lowest_grade = 100
     count = 0
     while(count<drop):
-        low_grade = 100
+        lowest_grade = 100
         for grade in grades:
             if grade <= low_grade:
-                low_grade = grade
-        grades.remove(low_grade)
+                lowest_grade = grade
+        grades.remove(lowest_grade)
         count+=1
     return sum(grades)/len(grades)
